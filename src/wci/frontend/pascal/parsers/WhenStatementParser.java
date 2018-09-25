4f16896fb4fed03c10772ed9b451c7078af58369
Lifeng Yuan <lifengwu@hotmail.com>
2018-09-25 13:58:01 -0700

    completed WHEN parser

--- src/wci/frontend/pascal/parsers/WhenStatementParser.java
+++ src/wci/frontend/pascal/parsers/WhenStatementParser.java
@@ -9,79 +9,116 @@ import wci.intermediate.icodeimpl.*;
 
 import static wci.frontend.pascal.PascalTokenType.*;
 import static wci.frontend.pascal.PascalErrorCode.*;
-import static wci.intermediate.icodeimpl.ICodeNodeTypeImpl.*;
-import static wci.intermediate.icodeimpl.ICodeKeyImpl.*;
+
 
 /**
- * <h1>WhileStatementParser</h1>
+ * <h1>WhenStatementParser</h1>
  *
- * <p>Parse a Pascal When statement.</p>
+ * <p>
+ * Parse a Made-up Pascal WHEN statement.
+ * </p>
  *
- * <p>Copyright (c) 2009 by Ronald Mak</p>
- * <p>For instructional purposes only.  No warranties.</p>
+ * <p>
+ * Copyright (c) 2009 by Ronald Mak
+ * </p>
+ * <p>
+ * For instructional purposes only. No warranties.
+ * </p>
  */
-public class WhenStatementParser extends StatementParser
-{
-    /**
-     * Constructor.
-     * @param parent the parent parser.
-     */
-    public WhenStatementParser(PascalParserTD parent)
-    {
-        super(parent);
-    }
+public class WhenStatementParser extends StatementParser {
+	/**
+	 * Constructor.
+	 * 
+	 * @param parent the parent parser.
+	 */
+	public WhenStatementParser(PascalParserTD parent) {
+		super(parent);
+	}
 
-    // Synchronization set for DO.
-    private static final EnumSet<PascalTokenType> NEXT_SET =
-        StatementParser.STMT_START_SET.clone();
-    static {
-        NEXT_SET.add(NEXT);
-        NEXT_SET.addAll(StatementParser.STMT_FOLLOW_SET);
-    }
+	// Synchronization set for NEXT.
+	private static final EnumSet<PascalTokenType> NEXT_SET = StatementParser.STMT_START_SET.clone();
+	static {
+		NEXT_SET.add(NEXT);
+		NEXT_SET.addAll(StatementParser.STMT_FOLLOW_SET);
+	}
 
-    /**
-     * Parse an IF statement.
-     * @param token the initial token.
-     * @return the root node of the generated parse tree.
-     * @throws Exception if an error occurred.
-     */
-    public ICodeNode parse(Token token)
-        throws Exception
-    {
-        token = nextToken();  // consume the IF
+	/**
+	 * Parse an WHEN statement.
+	 * 
+	 * @param token the initial token.
+	 * @return the root node of the generated parse tree.
+	 * @throws Exception if an error occurred.
+	 */
+	public ICodeNode parse(Token token) throws Exception {
 
-        // Create an IF node.
-        ICodeNode ifNode = ICodeFactory.createICodeNode(ICodeNodeTypeImpl.IF);
+		ExpressionParser expressionParser = new ExpressionParser(this);
+		StatementParser statementParser = new StatementParser(this);
+		// consume the WHEN
+		token = nextToken();
+		// Create an IF node.
+		ICodeNode ifNode = ICodeFactory.createICodeNode(ICodeNodeTypeImpl.IF);
 
-        // Parse the expression.
-        // The IF node adopts the expression subtree as its first child.
-        ExpressionParser expressionParser = new ExpressionParser(this);
-        ifNode.addChild(expressionParser.parse(token));
+		// save the reference to the root IF node
+		ICodeNode rootIfNode = ifNode;
 
-        // Synchronize at the THEN.
-        token = synchronize(NEXT_SET);
-        if (token.getType() == NEXT) {
-            token = nextToken();  // consume the =>
-        }
-        else {
-            errorHandler.flag(token, MISSING_NEXT, this);
-        }
+		while (true) {
+			// Parse the conditional expression before =>
+			// The IF node adopts the expression subtree as its first child.
 
-        // Parse the THEN statement.
-        // The IF node adopts the statement subtree as its second child.
-        StatementParser statementParser = new StatementParser(this);
-        ifNode.addChild(statementParser.parse(token));
-        token = currentToken();
+			ifNode.addChild(expressionParser.parse(token));
 
-        // Look for an ELSE.
-        if (token.getType() == OTHERWISE) {
-            token = nextToken();  // consume the THEN
+			token = currentToken();
+			// consume the =>
+			if (token.getType() == NEXT) {
+				token = nextToken();
+			} else {
+				errorHandler.flag(token, MISSING_NEXT, this);
+			}
 
-            // Parse the ELSE statement.
-            // The IF node adopts the statement subtree as its third child.
-            ifNode.addChild(statementParser.parse(token));
-        }
+			// Parse the statement after =>
+			// The IF node adopts the statement subtree as its second child.
+			ifNode.addChild(statementParser.parse(token));
 
-        return ifNode;
-    }
+			token = currentToken();
+			// consume the ;
+			if (token.getType() == SEMICOLON) {
+				token = nextToken();
+			} else {
+				errorHandler.flag(token, MISSING_SEMICOLON, this);
+			}
+
+			// Look if there is an OTHERWISE.
+			if (token.getType() == OTHERWISE) {
+
+				// consume the OTHERWISE
+				token = nextToken();
+
+				// consume the => in OTHERWISE clause
+				if (token.getType() == NEXT) {
+					token = nextToken();
+				} else {
+					errorHandler.flag(token, MISSING_NEXT, this);
+				}
+				// parse the statement after =>
+				// The IF node adopts the statement subtree as its third child.
+				ifNode.addChild(statementParser.parse(token));
+				token = currentToken();
+				// consume the END
+				if (token.getType() == END) {
+					token = nextToken();
+				} else {
+					errorHandler.flag(token, MISSING_END, this);
+				}
+				break;
+				// 'OTHERWISE' not found
+			} else {
+				// Create an new IF node to be adopted as the third child of parent IF node
+
+				ICodeNode childIfNode = ICodeFactory.createICodeNode(ICodeNodeTypeImpl.IF);
+				ifNode.addChild(childIfNode);
+				ifNode = childIfNode;
+			}
+		}
+		return rootIfNode;
+	}
 }
